73b745753e080a063b756f3f6a0da5058ca75833
Swagat Raj <shanu.r9971@gmail.com>
2021-10-06 01:10:03 +0530

    Add files via upload

---
+++ em/script.js
@@ -0,0 +1,5 @@
+function calc() {
+    var v = document.getElementById('v').ariaValueMax;
+    var a = document.getElementById('a').ariaValueMax;
+    var w = document.getElementById('w').ariaValueMax;
+}
