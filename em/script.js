21f0c1e8521dcdea0d2f92376e42189458697654
Swagat Raj <shanu.r9971@gmail.com>
2021-10-06 01:15:58 +0530

    Delete em directory

--- em/script.js
+++
@@ -1,5 +0,0 @@
-function calc() {
-    var v = document.getElementById('v').ariaValueMax;
-    var a = document.getElementById('a').ariaValueMax;
-    var w = document.getElementById('w').ariaValueMax;
-}
