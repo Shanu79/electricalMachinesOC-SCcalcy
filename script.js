bd06b28b640afc1a451e6f0154e29ee4598641cb
Swagat Raj <shanu.r9971@gmail.com>
2021-10-08 02:55:13 +0530

    tweaks
    added results tab,
    design overhaul
    code cleaning

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function calc1() {
-    var vo = document.getElementById('vo').ariaValueMax;
-    var io = document.getElementById('io').ariaValueMax;
-    var wo = document.getElementById('wo').ariaValueMax;
+    var vo = document.getElementById('vo').value;
+    var io = document.getElementById('io').value;
+    var wo = document.getElementById('wo').value;
 
     var cos1=parseFloat(wo)/(parseFloat(io)*parseFloat(vo));
     var sin1=Math.sqrt(1- Math.pow(parseFloat(cos1), 2));
@@ -11,7 +11,11 @@ function calc1() {
 
     if(!isNaN(ro))
     {
-        document.getElementById("ans").innerHTML="Ro is "+ro;
+        document.getElementById("ans").innerHTML="The equivalent exciting resistance is "+ro;
+    }
+    else if(!isNaN(xo))
+    {
+        document.getElementById("ans").innerHTML="The equivalent exciting reactance is "+xo;
     }
 
 }
@@ -23,4 +27,13 @@ function calc2() {
     var rc=parseFloat(wc)/Math.pow(parseFloat(ic), 2);
     var zc=parseFloat(vc)/parseFloat(ic);
     var xc=Math.sqrt(Math.pow(parseFloat(zc), 2) - Math.pow(parseFloat(rc), 2));
+
+    if(!isNaN(rc))
+    {
+        document.getElementById("ans").innerHTML="The equivalent resistance referred to the secondary side is "+rc;
+    }
+    else if(!isNaN(xc))
+    {
+        document.getElementById("ans").innerHTML="The equivalent reactance referred to the secondary side is given by "+xc;
+    }
 }
