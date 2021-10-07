21eaa36ac183f8c8cf3963c8432f26151dc4bd24
Swagat Raj <shanu.r9971@gmail.com>
2021-10-07 18:34:02 +0530

    small changes

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function calc() {
+function calc1() {
+    var v = document.getElementById('v').ariaValueMax;
+    var a = document.getElementById('a').ariaValueMax;
+    var w = document.getElementById('w').ariaValueMax;
+}
+function calc2() {
     var v = document.getElementById('v').ariaValueMax;
     var a = document.getElementById('a').ariaValueMax;
     var w = document.getElementById('w').ariaValueMax;
