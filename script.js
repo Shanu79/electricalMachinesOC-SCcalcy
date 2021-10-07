5b959ad0c0a8bbc1ba6dfca34afc384961f5406d
Swagat Raj <shanu.r9971@gmail.com>
2021-10-08 01:47:48 +0530

    added main calculation code

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function calc1() {
-    var v = document.getElementById('v').ariaValueMax;
-    var a = document.getElementById('a').ariaValueMax;
-    var w = document.getElementById('w').ariaValueMax;
+    var vo = document.getElementById('vo').ariaValueMax;
+    var io = document.getElementById('io').ariaValueMax;
+    var wo = document.getElementById('wo').ariaValueMax;
+
+    var cos1=parseFloat(wo)/(parseFloat(io)*parseFloat(vo));
+    var sin1=Math.sqrt(1- Math.pow(parseFloat(cos1), 2));
+
+    var ro=parseFloat(vo)/(parseFloat(io)*parseFloat(cos1));
+    var xo=parseFloat(vo)/(parseFloat(io)*parseFloat(sin1));
+
 }
 function calc2() {
-    var v = document.getElementById('v').ariaValueMax;
-    var a = document.getElementById('a').ariaValueMax;
-    var w = document.getElementById('w').ariaValueMax;
+    var vc = document.getElementById('vc').ariaValueMax;
+    var ic = document.getElementById('ic').ariaValueMax;
+    var wc = document.getElementById('wc').ariaValueMax;
+
+    var rc=parseFloat(wc)/Math.pow(parseFloat(ic), 2);
+    var zc=parseFloat(vc)/parseFloat(ic);
+    var xc=Math.sqrt(Math.pow(parseFloat(zc), 2) - Math.pow(parseFloat(rc), 2));
 }
